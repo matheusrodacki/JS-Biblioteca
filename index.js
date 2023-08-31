4b96c2434e348643a35a11fa5a09fad249ba4a2f
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-08-31 09:40:46 +0000

    importing file

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
+function pegaArquivo(caminhoArquivo){
+    const encoding = 'utf-8';
+    fs.readFile(caminhoArquivo, encoding, (erro, texto) => {
+        console.log(chalk.green(texto));
+    });
+}
 
-console.log(chalk.blue('olá mundo'));
-
-console.log('A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.');
-
-console.log('São geralmente recuperados a partir de um objeto ');
-
-
-//console.log(chalk.blue.bgWhite.bold('Alura'));
-////receber múltiplos argumentos
-//console.log(chalk.blue('curso', 'de', 'Node.js'));
-////métodos aninhados
-//console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
-//// uso de template strings e placeholders
-//console.log(`
-//CPU: ${chalk.red('90%')}
-//RAM: ${chalk.green('40%')}
-//DISK: ${chalk.yellow('70%')}
-//`);
+pegaArquivo('./arquivos/texto.md');
