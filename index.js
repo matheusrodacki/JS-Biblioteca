80204f27d00390274ae5f8046173cc109fae314d
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-08-31 09:30:36 +0000

    import chark to project

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+
+
+console.log(chalk.blue('olá mundo'));
+
+console.log('A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.');
+
+console.log('São geralmente recuperados a partir de um objeto ');
+
+
+//console.log(chalk.blue.bgWhite.bold('Alura'));
+////receber múltiplos argumentos
+//console.log(chalk.blue('curso', 'de', 'Node.js'));
+////métodos aninhados
+//console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
+//// uso de template strings e placeholders
+//console.log(`
+//CPU: ${chalk.red('90%')}
+//RAM: ${chalk.green('40%')}
+//DISK: ${chalk.yellow('70%')}
+//`);
