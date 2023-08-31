479641796c708f6edbcc93f9291bbd7b3932e9e0
matheusrodacki <matheus.rodacki@outlook.com>
2023-08-31 15:46:59 -0300

    Read parameters and error handling

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,33 @@
 import chalk from "chalk";
+import fs from "fs";
 import pegaArquivo from "./index.js";
 
-const caminho = process.argv;
+const argumentos = process.argv;
 
-async function processaTexto(caminho) {
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow ("lista de links:" ),resultado);
+function imprimeLista(resultado,arquivo = '') {
+  console.log("Lista de links: ",
+  chalk.black.bgGreen(arquivo),
+  resultado);
 }
 
-processaTexto(caminho);
+async function processaTexto(argumentos) {
+  try {
+    const parametro = argumentos[2];
+    if (fs.lstatSync(parametro).isFile()) {
+      const resultado = await pegaArquivo(parametro);
+      imprimeLista(resultado);
+    } else if (fs.lstatSync(parametro).isDirectory) {
+      const arquivos = await fs.promises.readdir(parametro);
+      arquivos.forEach(async (nomeDosArquivos) => {
+        const lista = await pegaArquivo(`${parametro}/${nomeDosArquivos}`);
+        imprimeLista(lista, nomeDosArquivos);
+      });
+    }
+  } catch(erro) {
+    if(erro.code === 'ENOENT'){
+        console.log(chalk.red(`Arquivo ou Diretório não existe! - Cód.: ${erro.code}`));
+    }
+  }
+}
+
+processaTexto(argumentos);
--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ function extraiLinks(texto) {
 }
 
 function trataErro(erro) {
-  console.log(erro);
   throw new Error(chalk.red(erro.code, "não há arquivo no diretório"));
 }
 
